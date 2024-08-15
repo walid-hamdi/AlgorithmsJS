6cb813bb69cc677cf4dc6084b0895999f4567984
walid-hamdi <walidhamdidev@gmail.com>
2024-08-15 07:49:25 +0100

    Solve maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const dic = {};
+  for (let char of str.split("")) {
+    if (char in dic) dic[char]++;
+    else dic[char] = 1;
+  }
+
+  var max = 0;
+  var maxChar = "";
+  for (let char in dic) {
+    if (dic[char] > max) {
+      max = dic[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
