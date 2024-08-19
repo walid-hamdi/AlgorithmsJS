3266f688b2b49dc6779f81856e8e2d9f5524955f
walid-hamdi <walidhamdidev@gmail.com>
2024-08-19 07:45:49 +0100

    Add second solution to solve anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,20 +9,42 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const word1 = stringA
-    .replace(/[^\w]/g, "")
-    .toLowerCase()
-    .split("")
-    .sort()
-    .join();
-  const word2 = stringB
-    .replace(/[^\w]/g, "")
-    .toLowerCase()
-    .split("")
-    .sort()
-    .join();
+  // # SECOND SOLUTION
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
 
-  return word1 === word2;
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length)
+    return false;
+
+  for (let key in aCharMap) {
+    if (aCharMap[key] !== bCharMap[key]) return false;
+  }
+
+  return true;
+
+  // # FIRST SOLUTION
+  //   const word1 = stringA
+  //     .replace(/[^\w]/g, "")
+  //     .toLowerCase()
+  //     .split("")
+  //     .sort()
+  //     .join();
+  //   const word2 = stringB
+  //     .replace(/[^\w]/g, "")
+  //     .toLowerCase()
+  //     .split("")
+  //     .sort()
+  //     .join();
+
+  //   return word1 === word2;
+}
+
+function buildCharMap(str) {
+  const dic = {};
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    dic[char] = str[char] + 1 || 1;
+  }
+  return dic;
 }
 
 module.exports = anagrams;
