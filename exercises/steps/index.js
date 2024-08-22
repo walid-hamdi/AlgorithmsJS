52c749ecf80d79429a8c9616642267cbb0e18b5c
walid-hamdi <walidhamdidev@gmail.com>
2024-08-22 07:06:41 +0100

    Solve steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,19 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for(let row=0 ; row<n ; row++){
+    let stair = "";
+    for(let column=0; column<n ; column++){
+      if(column <= row)
+        stair += "#"
+      else 
+        stair += " "
+    }
+    console.log(stair)
+  } 
+}
+
+steps(3);
 
 module.exports = steps;
