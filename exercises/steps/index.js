48158d74152e1f4c8a827c095a61a29b3b1f86d0
walid-hamdi <walidhamdidev@gmail.com>
2024-08-23 07:27:12 +0100

    Second solution to steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,17 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for(let row=0 ; row<n ; row++){
-    let stair = "";
-    for(let column=0; column<n ; column++){
-      if(column <= row)
-        stair += "#"
-      else 
-        stair += " "
-    }
-    console.log(stair)
-  } 
+// function steps(n) {
+//   for(let row=0 ; row<n ; row++){
+//     let stair = "";
+//     for(let column=0; column<n ; column++){
+//       if(column <= row)
+//         stair += "#"
+//       else
+//         stair += " "
+//     }
+//     console.log(stair)
+//   }
+// }
+
+steps(5,0,"") // "#"
+steps(5,0,"#") // " "
+steps(5,0, "# ") // " "
+steps(5,0,"#  ") // " "
+steps(5,"#   ") // " "
+steps(5,0,"#    ") // => "#    "
+steps(5,1,"") // "#"
+steps(5,1,"#") // "#"
+steps(5,1,"##") // " "
+steps(5,1,"## ") // " "
+steps(5,1,"##  ") // " "
+steps(5,1,"##   ") // => "##   " 
+steps(5,1,"### ") // " "
+// ...
+
+
+function steps(n, row = 0, stair = "") {
+  if (n === row) return;
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+  if (stair.length <= row) stair += "#";
+  else stair += " ";
+
+  steps(n, row, stair);
 }
 
 steps(3);
